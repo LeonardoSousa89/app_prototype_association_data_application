7dd277706a44bf001ac38b67cd5f75460fdd295a
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-02-27 23:08:55 -0300

    master

--- index/index.js
+++ index/index.js
@@ -1,17 +1,17 @@
 const doc = document
 
 $('#login').click(function(){
-    let username = doc.getElementById('email').value
+    let email = doc.getElementById('email').value
     let pass     = doc.getElementById('pass').value
 
     const url = 'http://192.168.100.20:8081/login'
 
-    const user = {username, pass}
+    const data = {email, pass}
 
     const config = {
        method:'POST',
        mode:'cors',
-       body: JSON.stringify(user),
+       body: JSON.stringify(data),
         headers:{
                 'Content-Type':'application/json'
         }
@@ -20,15 +20,17 @@ $('#login').click(function(){
    fetch(url,config)
         .then(response => {
             
-            if(response.OK)
+            if(response.ok)
             {
-                doc.location.href = '192.168.100.20:80/application/index.html'
+                doc.location.href = 'http://192.168.100.20:80/application/index.html'
             }
             else
             {
                 console.log(response.statusText)
             }
         })
-        .catch()
+        .catch((error)=>{
+                    console.log(error)
+        })
 
 })
